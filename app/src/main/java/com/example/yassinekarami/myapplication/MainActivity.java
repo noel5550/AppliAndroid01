3d30c00ab1d5c531e92a5b588d0f4274915e6e3d
yassinekarami <yassine.karami@outlook.com>
2019-01-18 22:49:56 +0000

    assemblage

--- app/src/main/java/com/example/yassinekarami/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yassinekarami/myapplication/MainActivity.java
@@ -1,199 +1,48 @@
 package com.example.yassinekarami.myapplication;
 
 import android.Manifest;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.telephony.SmsManager;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Button;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    SmsManager smsManager;
-    String message ="bonjour";
+    Timer timer;
 
-    private final String numero = "0658406185";
-    //private final String numero = "0650664099";
-    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
-    private static final int MY_PERMISSIONS_REQUEST_GPS_LOCATION = 1;
-    private static final int MY_PERMISSIONS_REQUEST_INTERNET = 1;
-
-
-    LocationManager locationManager;
-    double myLatitude;
-    double myLongitude;
-    String adress = "";
-    LocationListener locationListener;
-    boolean sendFlag = false;
-
-    ConnectivityManager connManager;
-    NetworkReceiver netReceiver;
-
-
+    Button btnExit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView)findViewById(R.id.text_view);
-        smsManager = SmsManager.getDefault();
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        btnExit = (Button)findViewById(R.id.btnExit);
 
 
-        connManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        netReceiver = new NetworkReceiver(); // network event
-
-        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-        registerReceiver(netReceiver,filter);
-
-        textView.setText(numero);
-
-
-        // permission pour envoyé des message
-        if (!checkPermission(Manifest.permission.SEND_SMS))
-        {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.SEND_SMS},
-                    MY_PERMISSIONS_REQUEST_SEND_SMS);
-        }
-
-        // permission pour accéder au GPS
-        if (!checkPermission(Manifest.permission.ACCESS_FINE_LOCATION))
-        {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
-                    MY_PERMISSIONS_REQUEST_GPS_LOCATION);
-        }
-
-        // récupération des donnés GPS
-        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION ))
-        {
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            locationListener = new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-
-                    myLatitude = location.getLatitude();
-                    myLongitude = location.getLongitude();
-                    adress = getAdress(myLatitude,myLongitude);
-                    message = getAdress(myLatitude,myLongitude);
-                    textView.append(adress);
-                    if (!sendFlag)
-                    {
-                        smsManager = SmsManager.getDefault();
-                        smsManager.sendTextMessage(numero, null, message, null, null);
-                        sendFlag = true;
-                    }
-                }
-
-                @Override
-                public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                }
-
-                @Override
-                public void onProviderEnabled(String provider) {
-
-                }
-
-                @Override
-                public void onProviderDisabled(String provider) {
-                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                    startActivity(intent);
-                }
-            };
-
-            locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
-        }
-
-
-    }
-
-    public void SendMessageClick(View view){
-
-        // on récupère le message a envoyé
-
-        smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(numero, null, message, null, null);
-    }
-
-
-    private boolean checkPermission(String permission)
-    {
-        int ok = ContextCompat.checkSelfPermission(this,permission);
-        if (ok ==  PackageManager.PERMISSION_GRANTED)
-            return true;
-
-        return false;
-    }
-
-    private String getAdress(double latitude, double longitude)
-    {
-        String adress ="";
-        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
-        try
-        {
-            List<Address> adresseList = geocoder.getFromLocation(latitude,longitude,1);
-            adress = adresseList.get(0).getAddressLine(0);
-        }catch(IOException e )
-        {
-            textView.setText("erreur localisation");
-        };
-        return adress;
-    }
-
-    // class permettant d'utiliser l'evenement onReceive
-    public class NetworkReceiver extends BroadcastReceiver {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
-            NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
-            if (networkInfo != null)
-            {
-                boolean wifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected();
-                boolean mobileData = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnected();
-                if (wifi)
-                {
-                    Toast.makeText(context,"wifi",Toast.LENGTH_SHORT).show();
-                }
-                else if (mobileData)
-                {
-                    Toast.makeText(context,"4g",Toast.LENGTH_SHORT).show();
-                }
+        // on instancie la classe timer
+        timer = new Timer();
+        //on utilise la fonction schedule qui permet de changer d'activité aprés un nombre de secondes
+        timer.schedule(new TimerTask() { // la classe timerTask permet de faire des actions, evenement aprés un certain temps
+            @Override
+            public void run() {
+                // on fait la transition d'activités, la classe intent permet de faire ce changement
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                // on commence l'activité de changement d'activité
+                startActivity(intent);
+                finish();
             }
-            else
-            {
-                Toast.makeText(context,"no",Toast.LENGTH_SHORT).show();
-
-            }
+        },5000);
+    }
 
 
-
-
-        }
+    public void exitClick(View view) {
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/yassinekarami/myapplication/SecondActivity.java
@@ -0,0 +1,147 @@
+package com.example.yassinekarami.myapplication;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView textView;
+    SmsManager smsManager;
+    String message ="bonjour";
+
+    private final String numero = "0658406185";
+    //private final String numero = "0650664099";
+
+
+    // verification des permissions
+    private static final int REQUEST_SEND_SMS = 1;
+    private static final int REQUEST_GPS_LOCATION = 1;
+
+    LocationManager locationManager;
+    double myLatitude;
+    double myLongitude;
+    String adress = "";
+    LocationListener locationListener;
+    boolean sendFlag = false;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        textView = (TextView)findViewById(R.id.text_view);
+        smsManager = SmsManager.getDefault();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        textView.setText(numero);
+
+        if (ContextCompat.checkSelfPermission(SecondActivity.this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},REQUEST_SEND_SMS);
+            recreate();
+
+        }
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION
+
+                    ,Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_GPS_LOCATION);
+
+            recreate();
+
+
+        }
+
+        // récupération des donnés GPS
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION ))
+        {
+
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+
+                    myLatitude = location.getLatitude();
+                    myLongitude = location.getLongitude();
+                    adress = getAdress(myLatitude,myLongitude);
+                    message = getAdress(myLatitude,myLongitude);
+
+                    if (!sendFlag)
+                    {
+                        smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(numero, null, message, null, null);
+                        sendFlag = true;
+                        finish();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            };
+
+            locationManager.requestLocationUpdates("gps", 1, 0, locationListener);
+        }
+
+
+    }
+    private boolean checkPermission(String permission)
+    {
+        int ok = ContextCompat.checkSelfPermission(this,permission);
+        if (ok ==  PackageManager.PERMISSION_GRANTED)
+            return true;
+
+        return false;
+    }
+
+    private String getAdress(double latitude, double longitude)
+    {
+        String adress ="";
+        Geocoder geocoder = new Geocoder(SecondActivity.this, Locale.getDefault());
+        try
+        {
+            List<Address> adresseList = geocoder.getFromLocation(latitude,longitude,1);
+            adress = adresseList.get(0).getAddressLine(0);
+        }catch(IOException e )
+        {
+            textView.setText("erreur localisation");
+        };
+        return adress;
+    }
+}
