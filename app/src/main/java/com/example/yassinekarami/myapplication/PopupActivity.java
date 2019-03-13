d34ae7901c0f42aece252f71c5a6e8274710f7f6
yassinekarami <yassine.karami@outlook.com>
2019-03-13 21:13:54 +0000

    popup avec les infos

--- app/src/main/java/com/example/yassinekarami/myapplication/PopupActivity.java
+++ app/src/main/java/com/example/yassinekarami/myapplication/PopupActivity.java
@@ -10,18 +10,6 @@ public class PopupActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popup);
-      //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
-
-       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
